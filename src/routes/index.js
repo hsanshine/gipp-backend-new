187eaf91905a988fe49c6635fff3c728ba3145cc
Hamza <sanshinehamza@gmail.com>
2022-04-10 09:48:47 +0900

    setup swagger to automate routes documentation

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,27 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
+/**
+ * @swagger
+ *  tags:
+ *    name: Testing
+ *    description: testing swagger
+ */
+
+/**
+ * @swagger
+ * /:
+ *  get:
+ *    summary: I am using this to test my api.
+ *    tags: [Testing]
+ *    responses:
+ *      200:
+ *          description: 'the test is a succcess'
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: string
+ */
 router.get("/", (req, res, next) => {
   res.status(200).json({ msg: "you have reached GIPP" });
 });
