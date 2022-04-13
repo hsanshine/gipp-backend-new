12a0c9eabbe9c3b3a3b4ec7ae036666a4b1eefad
Hamza <sanshinehamza@gmail.com>
2022-04-14 00:50:12 +0900

    fixed data leak error in config file to connect to db

--- config.js
+++ config.js
@@ -2,16 +2,6 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-`#mongo db user name
-MONGO_DB_USERNAME = 'hsanshine'
-
-#mongo db password
-MONGO_DB_PWD = 'qWJ7XOQD8uTMmOco'
-
-#mongo db name
-MONGO_DB_NAME = 'gipp-dev'
-`;
-
 module.exports = {
   db: {
     name: process.env.MONGO_DB_NAME,
